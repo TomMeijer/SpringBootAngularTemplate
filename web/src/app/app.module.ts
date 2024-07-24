564d8651cbbddebff0895f6001a32d27f1a3899e
Tom Meijer <tommeijer95@hotmail.com>
2024-07-24 21:06:39 +0200

    update to angular 18

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -12,13 +12,14 @@ import { RegisterComponent } from './component/register/register.component';
 import { HomeComponent } from './domain/home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import {PasswordStrengthMeterModule} from 'angular-password-strength-meter';
 import {RepeatedPasswordValidatorDirective} from './validator/repeated-password-validator.directive';
 import { ProfileComponent } from './domain/user/component/profile/profile.component';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import {PopoverModule} from 'ngx-bootstrap/popover';
 import {AuthInterceptor, ErrorInterceptor, TmBootstrapModule} from '@tommeijer/tm-bootstrap';
 import {environment} from '../environments/environment';
+import {PasswordStrengthMeterComponent} from "angular-password-strength-meter";
+import {provideZxvbnServiceForPSM} from "angular-password-strength-meter/zxcvbn";
 
 @NgModule({
   declarations: [
@@ -40,14 +41,15 @@ import {environment} from '../environments/environment';
     HttpClientModule,
     FormsModule,
     TmBootstrapModule,
-    PasswordStrengthMeterModule.forRoot(),
     ModalModule.forRoot(),
-    PopoverModule
+    PopoverModule,
+    PasswordStrengthMeterComponent
   ],
   providers: [
     {provide: 'apiUrl', useValue: environment.apiUrl},
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    provideZxvbnServiceForPSM()
   ],
   bootstrap: [AppComponent]
 })
