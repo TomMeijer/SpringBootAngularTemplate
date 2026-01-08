a3d4b98f3107debfa4cb0f1474ae1b061f81259c
Tom Meijer <tommeijer95@hotmail.com>
2026-01-08 16:22:11 +0800

    update footer link, and extend profile e2e tests

--- web/cypress/e2e/profile/profile.cy.js
+++ web/cypress/e2e/profile/profile.cy.js
@@ -11,4 +11,21 @@ context('Profile', () => {
     cy.get('[name=lastName] input').should('contain.value', 'User')
     cy.get('[name=email] input').should('contain.value', 'test@cypress.com')
   })
+
+  it('can update the current user\'s profile information', () => {
+    cy.wait('@getUser')
+    cy.get('[data-cy=profile-dropdown]').click()
+    cy.get('[data-cy=profile-link]').click()
+    cy.get('[name=firstName] input').clear().type('Hulk')
+    cy.get('[name=lastName] input').clear().type('Hogan')
+    cy.intercept('PUT', '/user', {}).as('updateUser');
+    cy.get('[data-cy=profile-save-btn] button').click()
+    cy.wait('@updateUser').then(interceptor => {
+      const body = interceptor.request.body;
+      expect(body).to.contain('name="firstName"');
+      expect(body).to.contain('\r\n\r\nHulk\r\n');
+      expect(body).to.contain('name="lastName"');
+      expect(body).to.contain('\r\n\r\nHogan\r\n');
+    })
+  })
 })
--- web/cypress/support/commands.ts
+++ web/cypress/support/commands.ts
@@ -12,7 +12,6 @@ declare global {
 
 Cypress.Commands.add('visitAsLoggedIn', (path: string = '/') => {
   cy.intercept('GET', '/user', { fixture: 'user.json' }).as('getUser');
-
   cy.visit(path, {
     onBeforeLoad(win) {
       win.localStorage.setItem('tm-access-token', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0bSIsImlhdCI6MTc2Nzg0OTYzNSwiZXhwIjoxOTg4Nzc0NDM4LCJhdWQiOiJ0ZXN0Iiwic3ViIjoidGVzdEBjeXByZXNzLmNvbSJ9.H1CV1bpGq6bbwVvWuFXzFlZqf2ySF8hr1JX_fUKAKy0');
